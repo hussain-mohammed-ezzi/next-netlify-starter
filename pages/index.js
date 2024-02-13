7bb26724ccb654813b2a729439f7135c9e1f82f1
hussain mohammed ezzi <109825958+hussain-mohammed-ezzi@users.noreply.github.com>
2024-02-13 20:45:41 +0500

    Update index.js fun

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next.js Starter! here is your friend hussain ezzi </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my app! your friend hussain ezzi is ever reafy to help you. He operates Munfasil Solutions" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/index.js</code> plz dont just copy, also use some of your brains too
         </p>
       </main>
 
